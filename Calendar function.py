bd9d2edd813f7a11dcfb3c39dd674d5282b43b34
Sarthak Gupta <sarthi.abhi@gmail.com>
2019-10-31 22:21:43 +0530

    Update Calendar function.py

--- Calendar function.py
+++ Calendar function.py
@@ -5,6 +5,9 @@ import calendar
 print ("The calender of year 2012 is : ") 
 print (calendar.calendar(2012,2,1,6)) 
   
+  
+  
 #using firstweekday() to print starting day number 
 print ("The starting day number in calendar is : ",end="") 
 print (calendar.firstweekday())
+
