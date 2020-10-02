ce0cc59fec4528784c7deeef6e1382f97f54c020
Sarthak Gupta <sarthi.abhi@gmail.com>
2020-10-02 23:49:38 +0530

    Removed hardcoded value and enabled user input for calender year

--- Calendar function.py
+++ Calendar function.py
@@ -1,9 +1,10 @@
 import calendar 
   
 # using calender to print calendar of year 
-# prints calendar of 2012 
-print ("The calender of year 2012 is : ") 
-print (calendar.calendar(2012,2,1,6)) 
+# prints calendar of the year input by user. 
+yr = (int)(input ("Enter Year :"))
+print ("The calender of year %d is : " % (yr)) 
+print (calendar.calendar(yr,2,1,6)) 
   
 #using firstweekday() to print starting day number 
 print ("The starting day number in calendar is : ",end="") 
