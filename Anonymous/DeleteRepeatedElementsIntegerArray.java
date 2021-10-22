8e2e24c49844bcc8662e927e95e5b6f0d1c70b6b
Sanchit Thacker <sanchit.thacker@gmail.com>
2021-10-23 00:58:06 +0530

    Create DeleteRepeatedElementsIntegerArray.java

---
+++ Anonymous/DeleteRepeatedElementsIntegerArray.java
@@ -0,0 +1,8 @@
+HashMap<Integer,Boolean> m =  new HashMap<>();
+    for (int j = 0; j < a.length); j++)
+         {
+            if (m.get(a[j]) == null) 
+                System.out.print(a[j] + " "); 
+                 mp.put(a[j], true); 
+        } 
+    } 
