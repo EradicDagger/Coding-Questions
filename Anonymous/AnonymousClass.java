10ef6f71fdae7b769f9e18a455ae2065be458efb
Manmeet Singh <singhmanmeet2222@gmail.com>
2021-10-22 21:36:28 +0530

    Create AnonymousClass.java

---
+++ Anonymous/AnonymousClass.java
@@ -0,0 +1,33 @@
+public java.util.Enumeration testMethod()
+
+{
+
+    return new java.util.Enumeration()
+
+    {
+
+        @Override
+
+        public boolean hasMoreElements()
+
+        {
+
+            // TODO Auto-generated method stub
+
+            return false;
+
+        }
+
+        @Override
+
+        public Object nextElement()
+
+        {
+
+            // TODO Auto-generated method stub
+
+            return null;
+
+        }
+
+    }
